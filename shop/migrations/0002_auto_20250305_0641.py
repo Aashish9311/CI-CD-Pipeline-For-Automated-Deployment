b337bf1b6fb2e04929d9ec0f4edf4c29a656d160
Aashish Kumar Jha <137982205+Aashish9311@users.noreply.github.com>
2025-05-28 12:32:15 +0530

    Update 0002_auto_20250305_0641.py

--- shop/migrations/0002_auto_20250305_0641.py
+++ shop/migrations/0002_auto_20250305_0641.py
@@ -17,7 +17,7 @@ from django.db import migrations
 from ..models import Product
 
 def add_products(apps, schema_editor):
-    Product.objects.create(name="Intern", description="A comfortable cotton shirt.", price=400)
+    Product.objects.create(name="Shirt", description="A comfortable cotton shirt.", price=400)
     Product.objects.create(name="Programming Python", description="A book about python programming.", price=120)
     Product.objects.create(name="Mountain Bike", description="A sturdy mountain bicycle.", price=3500)
     Product.objects.create(name="Wireless Mouse", description="A comfortable wireless mouse.", price=550) # Added this line
