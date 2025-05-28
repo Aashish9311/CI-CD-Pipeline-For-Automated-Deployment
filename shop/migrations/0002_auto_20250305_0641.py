f3f550e683058b7ba3c8c896d96ffed42ab477ea
Aashish Kumar Jha <137982205+Aashish9311@users.noreply.github.com>
2025-05-28 12:19:49 +0530

    Update 0002_auto_20250305_0641.py

--- shop/migrations/0002_auto_20250305_0641.py
+++ shop/migrations/0002_auto_20250305_0641.py
@@ -19,7 +19,7 @@ from ..models import Product
 def add_products(apps, schema_editor):
     Product.objects.create(name="Shirt", description="A comfortable cotton shirt.", price=400)
     Product.objects.create(name="Programming Python", description="A book about python programming.", price=120)
-    Product.objects.create(name="intern", description="A sturdy mountain bicycle.", price=3500)
+    Product.objects.create(name="Mountain Bike", description="A sturdy mountain bicycle.", price=3500)
     Product.objects.create(name="Wireless Mouse", description="A comfortable wireless mouse.", price=550) # Added this line
     Product.objects.create(name="Headphones", description="Over-ear noise-cancelling headphones.", price=5000)  # Added this line
     Product.objects.create(name="Intern",description="to learn deveops",price=free)
